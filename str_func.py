ebc8035fd4ae2bb1a1ba82b7abd4fe23d8da5aa0
MidnightFruit <81094799+MidnightFruit@users.noreply.github.com>
2024-03-18 22:00:37 +0700

    Добавлены функция first_to_up, комментарии

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def to_up(str):
+    """
+    Делает символы в строке заглавными
+    :param str: входная строка
+    :return:
+    """
     return str.upper()
+
+def first_to_up(string: str) -> str:
+    """
+    Делает так что бы каждое слово начиналось с заглавной буквы
+    :param string: входная строка
+    :return:
+    """
+    return string.title()
